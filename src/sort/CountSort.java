3fef0539b6778d00fb5a785ff1f323659bbb35df
byex_888 <604217454@qq.com>
2019-01-29 17:31:13 +0800

    Count sort.

--- src/sort/CountSort.java
+++ src/sort/CountSort.java
@@ -1,6 +1,13 @@
-import java.util.PrimitiveIterator;
+package sort;
 
-public class BucketSort {
+/**
+ * 计数排序是桶排序的特殊一种
+ *
+ * 计算排序只能用在数据范围不大的场景中，如果数据范围k要比排序的数据n大很多，就适合使用计算排序了
+ *
+ * 时间复杂度O(n)
+ */
+public class CountSort {
 
     public static void bucketSort(int[] array, int max) {
         int[] a = new int[max+1];
